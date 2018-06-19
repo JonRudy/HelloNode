e76bf90c18e5d2dbbe18f6016ff022fb88b3bf3b
EC2 Default User <ec2-user@ip-172-31-23-217.us-west-2.compute.internal>
2018-06-19 22:35:01 +0000

    Updating to inclue ip

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var temp = require('dotenv').config();
 var app = express();
+var mode   = process.env.NODE_ENV;
+var your_ip = process.env.YOUR_IP;
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send(`hi you are listening from  ` + your_ip);
 });
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
