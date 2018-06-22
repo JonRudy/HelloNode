d2df6eebe67cfe5d2f43a8a1753271111ee51c81
JonRudy <jrudy2@mail.csuchico.edu>
2018-06-22 16:18:40 -0700

    Adding Working Giphy API search

--- app.js
+++ app.js
@@ -3,13 +3,39 @@ var temp = require('dotenv').config();
 var app = express();
 var mode   = process.env.NODE_ENV;
 var your_ip = process.env.YOUR_IP;
+var http = require('http');
+var exphbs  = require('express-handlebars');
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+var http = require('http');
 
 app.get('/', function (req, res) {
-  res.send(`hi you are listening from  ` + your_ip);
-});
+  var queryString = req.query.term;
+  // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
+  var term = encodeURIComponent(queryString);
+  // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
+  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+
+  http.get(url, function(response) {
+    // SET ENCODING OF RESPONSE TO UTF8
+    response.setEncoding('utf8');
+
+    var body = '';
+
+    response.on('data', function(d) {
+      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
+      body += d;
+    });
+    response.on('end', function() {
+      // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
+      var parsed = JSON.parse(body);
+      // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+     res.render('home', {gifs: parsed.data, aws_ip: your_ip })
+    });
+  });
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
